0651df87149df5e137c822edefb0a4f790c672da
Armando Dollia <dollia.armando@gmail.com>
2017-03-16 10:56:28 -0500

    Implements environment

---
+++ public/js/application.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  // PSEUDO-CODE:
+  //   1- intercept the form submission event using jQuery
+  //   2- prevent the default action for that event from happening
+  //   3- use jQuery to submit an AJAX post to the form's action
+  //   4- when the AJAX post is done, display the new die roll using jQuery
+
+  $('.container').on('submit', 'form', function (event) {
+    event.preventDefault();
+    var data = $('form').serialize();
+    $.ajax({
+      url: 'rolls',
+      method: 'POST',
+      data: data
+    })
+        .done(function(roll) {
+      $("#die-container").html(roll);
+    });
+  });
+});
