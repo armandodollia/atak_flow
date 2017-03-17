45d0e69a56c0c44165d89d2b0106be5241879b5a
Karl Thomas <email": null>
2017-03-16 23:49:13 -0500

    create beahavioeur methods for question: solved?, solution

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,4 +7,15 @@ class Question < ActiveRecord::Base
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
   has_many :answers
+
+  #returns a boolean if select finds an answer marked as correct
+  def solved?
+    !!self.answers.select {|x| x.correct}[0]
+  end
+
+  #if solved, return the solution, else solution = nil
+  def solution
+    return self.answers.select {|x| x.correct}[0] if solved?
+    nil
+  end
 end
--- db/migrate/20170316115614_answers.rb
+++ db/migrate/20170316115614_answers.rb
@@ -2,9 +2,10 @@ class Answers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.string   :body, null: false
+      t.boolean  :correct, default: :false
       t.integer  :user_id, null: false
       t.integer  :question_id, null: false
-      
+
       t.timestamps null: false
     end
   end
