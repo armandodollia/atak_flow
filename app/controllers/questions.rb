9206be524f9e7dbe0e6270086a7c24415e603ae2
Karl Thomas <email": null>
2017-03-17 17:01:24 -0500

    implement working javascript

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -1,6 +1,6 @@
 get '/questions' do
   @questions = Question.all
-  erb :'questions/index'
+  erb :'/questions/index'
 end
 
 get '/questions/:question_id' do
@@ -12,7 +12,7 @@ get '/questions/:question_id/comments' do
   @question = current_question(params[:question_id])
   @comments = @question.comments
   if request.xhr?
-    erb :'comments/_index', layout: false
+    erb :'/comments/_index', layout: false
   else
     erb :'/comments/index'
   end
