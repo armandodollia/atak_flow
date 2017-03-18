98889c0cad53829dc08e87db80aad70033200e19
Armando Dollia <dollia.armando@gmail.com>
2017-03-18 18:06:22 -0500

    adds notes

--- app/controllers/vote.rb
+++ app/controllers/vote.rb
@@ -1,16 +1,16 @@
-post '/comments/:comment_id/votes' do
+put '/comments/:comment_id/votes' do
   update_vote(Comment.find_by(id: params[:comment_id].to_i))
 
   redirect back
   end
 
-post '/answers/:answer_id/votes' do
+put '/answers/:answer_id/votes' do
   update_vote(Answer.find_by(id: params[:answer_id].to_i))
 
   redirect back
 end
 
-post '/questions/:question_id/votes' do
+put '/questions/:question_id/votes' do
   update_vote(Question.find_by(id: params[:question_id].to_i))
   
   redirect back
