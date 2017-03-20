702d8f43897cc015076741c632fb155899554ac9
Karl Thomas <email": null>
2017-03-20 09:51:39 -0500

    eyyyyy

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -3,6 +3,25 @@ get '/questions' do
   erb :'/questions/index'
 end
 
+get '/questions/new' do
+  if logged_in?
+    erb :'/questions/new'
+  else
+    redirect back
+  end
+end
+
+post '/questions' do
+  redirect back if !logged_in?
+  new_question = Question.new(title: params[:title], body: params[:body], user_id: current_user.id)
+  if new_question.save
+    redirect "/questions"
+  else
+    @errors = new_question.errors.full_messages
+    erb :'/question/new'
+  end
+end
+
 get '/questions/:question_id' do
   @question = current_question(params[:question_id])
   erb :'/questions/show'
